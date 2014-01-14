e02b3c9158c91c46a4a70a4d8dc39264a37edf34
Thomas Plotkowiak <plotti@dhcp-wlan-uzh-89-206-82-145.uzh.ch>
2014-01-14 12:45:31 +0100

    session management

--- itunes.rb
+++ itunes.rb
@@ -1,27 +1,45 @@
 require 'appscript'; include Appscript
 require 'lastfm'
-itunes = app('iTunes')
-title_old = ""
-lastfm = Lastfm.new("447770d42fbd35cee967957143c434b2", "4afec6da9e6ee17be152697c0411b4d7")
-#token = lastfm.auth.get_token
-#token = "6827eb9ea211d2c508ca46ddb5d171fc"
-#open 'http://www.last.fm/api/auth/?api_key=447770d42fbd35cee967957143c434b2&token=235066c824aeedc6dbc7318839f1fb0c'
-#session = lastfm.auth.get_session(:token => token)['key']
-session = "e317c683a4e12d355aefa0db23af7916"
+
+#LastFM Setup
+api_key = "447770d42fbd35cee967957143c434b2"
+secret = "4afec6da9e6ee17be152697c0411b4d7"
+session = ""
+lastfm = Lastfm.new(api_key, secret)
+begin
+	session = File.open("last_fm_session.txt").first
+rescue
+	token = lastfm.auth.get_token
+	`open "http://www.last.fm/api/auth/?api_key=#{api_key}&token=#{token}"`
+	result = lastfm.auth.get_session(:token => token)['key']
+	while result == nil
+		result = lastfm.auth.get_session(:token => token)['key']
+		sleep(3)
+	end
+	session = result
+	File.open("last_fm_session.txt", 'w') { |file| file.write("#{session}")}
+end
 lastfm.session = session
 
+#Get Itunes
+itunes = app('iTunes')
+
+# Start Looping
+title_old = ""
 while true
 	title_new = itunes.current_stream_title.get
-	if title_new == title_old
-		sleep(10)
-	else
-		text = title_new.gsub(/\| FM4.*/, "")
-		artist = text.split(" - ").first
-		track = text.split(" - ").last
-		title_old = title_new 
-		if artist != "FM4: You're at home baby" || track != "FM4: You're at home baby"
-			puts "#{artist} - #{track}"
-			lastfm.track.scrobble(:artist => artist, :track => track)
+	if title_new != :missing_value
+		if title_new == title_old
+			sleep(10)
+		else
+			text = title_new.gsub(/\| FM4.*/, "")
+			artist = text.split(" - ").first
+			track = text.split(" - ").last
+			title_old = title_new 
+			if !artist.downcase.include?("fm4") && !track.downcase.include?("fm4")
+				puts "Scrobble: #{artist} - #{track}"
+				lastfm.track.scrobble(:artist => artist, :track => track)
+			end
 		end
 	end
 end
